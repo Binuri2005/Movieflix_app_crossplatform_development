ca63086091784a7353bd084de92f47036488213b
minsadi.rajasinghe@gmail.com <minsadi.rajasinghe@gmail.com>
2024-03-15 21:51:23 +0530

    firebase_authdart

--- lib/client/firebas_auth.dart
+++ lib/client/firebas_auth.dart
@@ -1,82 +1,94 @@
-// ignore_for_file: unnecessary_null_comparison
+// This class provides methods for authentication using Firebase Auth.
+import 'dart:async'; // Importing asynchronous Dart library.
 
-import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart'; // Importing Firebase Auth package.
+import 'package:movie_app_example/models/application_user.dart'; // Importing application user model.
 
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:movie_app_example/models/application_user.dart';
-
-import 'firebase_database_services.dart';
+import 'firebase_database_services.dart'; // Importing Firebase database services.
 
 class FirebaseAuthService {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseAuth _auth =
+      FirebaseAuth.instance; // Initializing FirebaseAuth instance.
 
+  // Get the current user's authentication state.
   Stream<User?> get user {
     // signInWithEmailAndPassword('mgmaheshnew@gmail.com', 'pfU3LGjq5vAxKtF');
-    return _auth.authStateChanges();
+    return _auth
+        .authStateChanges(); // Returning authentication state changes stream.
   }
 
+  // Method to sign in anonymously.
   Future<User?> signInAnon() async {
     try {
+      // Signing in anonymously.
       UserCredential result = await _auth.signInAnonymously();
-      User user = result.user!;
-      return user;
+      User user = result.user!; // Getting user from result.
+      return user; // Returning user.
     } catch (e) {
-      print(e.toString());
-      return null;
+      print(e.toString()); // Printing error if any occurs.
+      return null; // Returning null if error occurs.
     }
   }
 
+  // Method to sign in with email and password.
   Future<User?> signInWithEmailAndPassword(
       String email, String password) async {
     try {
+      // Signing in with email and password.
       UserCredential result = await _auth.signInWithEmailAndPassword(
           email: email, password: password);
-      User user = result.user!;
-      return user;
+      User user = result.user!; // Getting user from result.
+      return user; // Returning user.
     } catch (error) {
-      print(error.toString());
-      return null;
+      print(error.toString()); // Printing error if any occurs.
+      return null; // Returning null if error occurs.
     }
   }
 
+  // Method to edit user profile.
   Future<User?> editUser(
       {required String email,
       required String password,
       required String displayName,
       required String photoURL}) async {
     try {
+      // Signing in with email and password.
       UserCredential result = await _auth.signInWithEmailAndPassword(
           email: email, password: password);
-      User user = result.user!;
+      User user = result.user!; // Getting user from result.
 
+      // Updating user profile.
       await user?.updateProfile(displayName: displayName, photoURL: photoURL);
 
-      return user;
+      return user; // Returning user.
     } catch (error) {
-      print(error.toString());
-      return null;
+      print(error.toString()); // Printing error if any occurs.
+      return null; // Returning null if error occurs.
     }
   }
 
+  // Method to register with email and password.
   Future<User?> registerWithEmailAndPassword(
       String email, String password) async {
     try {
+      // Creating user with email and password.
       UserCredential result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
-      User user = result.user!;
-      return user;
+      User user = result.user!; // Getting user from result.
+      return user; // Returning user.
     } catch (error) {
-      print(error.toString());
-      return null;
+      print(error.toString()); // Printing error if any occurs.
+      return null; // Returning null if error occurs.
     }
   }
 
+  // Method to sign out.
   Future signOut() async {
     try {
-      return await _auth.signOut();
+      return await _auth.signOut(); // Signing out.
     } catch (error) {
-      print(error.toString());
-      return null;
+      print(error.toString()); // Printing error if any occurs.
+      return null; // Returning null if error occurs.
     }
   }
 }
